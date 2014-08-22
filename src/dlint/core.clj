a7ff4d21ed5101a73cd0f989d2590c18a168df30
Gabriel Horner <gabriel.horner@gmail.com>
2014-08-22 09:22:30 -0400

    First pass at linting

--- src/dlint/core.clj
+++ src/dlint/core.clj
@@ -1,1 +1,61 @@
-(ns dlint.core)
+(ns dlint.core
+  (:require [clojure.set :as cset]))
+
+;; thanks to datascript
+(defn- parse-query [query]
+  (loop [parsed {} key nil qs query]
+    (if-let [q (first qs)]
+      (if (keyword? q)
+        (recur parsed q (next qs))
+        (recur (update-in parsed [key] (fnil conj []) q) key (next qs)))
+      parsed)))
+
+(defn- variable? [sym]
+  (and (symbol? sym) (re-find #"^\?" (str sym))))
+
+(defn- ->symbols [values]
+  (set (filter variable? (flatten values))))
+
+(defn- symbol-diff [v1 & others]
+  (apply cset/difference
+         (->symbols v1)
+         (map ->symbols others)))
+
+(defn- lint-map-key
+  "Lints map at given key and returns set of unbound variables"
+  [map-key lint-map]
+  (let [tuples (get lint-map map-key)
+        tuples (if (sequential? (first tuples)) tuples (list tuples))
+        other-key-vals (map #(get lint-map %)
+                            (disj (set (keys lint-map)) map-key))]
+    (apply cset/union
+           (map #(apply symbol-diff %
+                        (into other-key-vals
+                              (disj (set (get lint-map map-key)) %)))
+                tuples))))
+
+(defn- ->unused-variables [lint-map]
+  (->> (keys lint-map)
+       (map #(vector % (lint-map-key % lint-map)))
+       (remove (comp empty? second))
+       (into {})))
+
+(defn- lint-rules [rules]
+  (let [rules-map (->> rules (map-indexed #(vector %1 %2)) (into {}))
+        key-map (->> rules (map-indexed #(vector %1 (keyword (ffirst %2)))) (into {}))]
+    (reduce-kv
+     (fn [accum k v]
+       (update-in accum [(get key-map k)]
+                  (fnil into #{}) v))
+     {}
+     (->unused-variables rules-map))))
+
+(defn lint
+  "Lints a datomic-style datalog query or rule. For queries, returns a map of
+  query keys to sets of unbound variables e.g. {:find #{'?entity}}.
+  For rules, returns a map of rule names to sets of unbound variables e.g.
+  {:some-rule #{'?aarg1}}"
+  [query-or-rule]
+  (if (and (sequential? (first query-or-rule)) (sequential? (ffirst query-or-rule)))
+    (lint-rules query-or-rule)
+    (->unused-variables (if (map? query-or-rule) query-or-rule (parse-query query-or-rule)))))
--- test/dlint/core_test.clj
+++ test/dlint/core_test.clj
@@ -1,7 +1,47 @@
 (ns dlint.core-test
     (:require [clojure.test :refer :all]
-              [dlint.core :refer :all]))
+              [dlint.core :refer [lint]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest lint-basic-queries
+  (testing "query map"
+    (is (= {:find #{'?a} :where #{'?e}}
+           (lint {:find '[?a] :in '[$ ?tag] :where '[[?e :name ?tag]]}))))
+  (testing "no typos"
+    (is (= {}
+           (lint '[:find ?e :in $ ?tag :where [?e :name ?tag]]))))
+  (testing "typo b/n :in and :where"
+    (is (= {:in #{'?name} :where #{'?named}}
+           (lint '[:find ?e :in $ ?name
+                   :where [?e :tags ?tag] [?tag :name ?named]]))))
+  (testing "typo b/n :find and :in"
+    (is (= {:find #{'?heads} :in #{'?heards}}
+           (lint '[:find (sum ?heads) :with ?monster :in [[?monster ?heards]]]))))
+  (testing "typo b/n :in and :with"
+    (is (= {:in #{'?monsterd} :with #{'?monster}}
+           (lint '[:find (sum ?heads) :with ?monster :in [[?monsterd ?heads]]])))))
+
+(deftest lint-intermediate-queries
+  (testing "typo in :find aggregator"
+    (is (= {:find #{'?a} :where #{'?e}}
+           (lint '[:find (count ?a) :in $ ?tag :where [?e :name ?tag]]))))
+  (testing "typo in :where function"
+    (is (= {:where #{'?t '?t2}}
+           (lint '[:find ?e :in $ ?tag
+                   :where [?e :name ?t] [(name ?tag) ?t2]]))))
+  (testing "typo in :where rule"
+    (is (= {:where #{'?t '?t2}}
+           (lint '[:find ?e :in $ ?tag
+                   :where [?e :name ?t] [(some-rule ?t2 ?tag)]])))))
+
+(def rules
+  (concat '[[(tagged-with ?e ?name)
+             [?e :tags ?tag]
+             [?tag :name ?name]]]
+          '[[(name-or-alias ?e1 ?name)
+             [?e :name ?name]]
+            [(name-or-alias ?e ?name)
+             [?e2 :alias ?name]]]))
+
+(deftest lint-rules
+  (is (= {:name-or-alias #{'?e1 '?e2}}
+         (lint rules))))
